551ca03f4bf8da826428569b8322cb1ee21519b9
Doomhyena <91425554+doomhyena@users.noreply.github.com>
2024-09-21 09:21:19 +0200

    Első feladat

---
+++ .history/00.Home/03.harmadik_20240921091628.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<h1>Gyakorló Feladatsor</h1>";
+/*
+
+1. Feladat
+
+    Feladat: Hozz létre két változót, $a = 15 és $b = 30.
+        Ha az $a kisebb, mint $b, írasd ki, hogy "$a kisebb, mint $b".
+        Ha $a nagyobb, mint $b, írasd ki, hogy "$a nagyobb, mint $b".
+        Ha a két szám egyenlő, írasd ki, hogy "$a és $b egyenlő".
+
+*/
+
+echo "<h2>.Feladat</h2>";
