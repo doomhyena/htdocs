f2c0d7055ac91199545ffa2324e4fcd9ee163f6e
Doomhyena <91425554+doomhyena@users.noreply.github.com>
2024-09-24 13:23:06 +0200

    Kommentek
    Kommentek

--- .history/00.Home/hehe_20240924132225.php
+++ .history/00.Home/hehe_20240924132225.php
@@ -7,17 +7,3 @@ echo "<br>"; // Sortörés, ajánlatos, hogy ne folyon minden egybe
 echo 55; // Számokat ki lehet anélkül is íratni
 echo "<br>"; 
 echo 5+5; // Műveleteket is ki lehet így íratni, viszont ha macskakörömbe rakjuk őket akkor nem adja össze, hanem csak sima szövegként jeleníti meg
-
-// Kommentelés
-
-// ez egy soros komment
-
-/*
-
-ez
-egy 
-több
-soros
-komment
-
-*/
