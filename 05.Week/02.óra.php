8356d6b9b20199aa9729aa28713dc1a006221cc7
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-10-07 15:42:21 +0200

    01.Feladat leírás

--- 05.Week/02.óra.php
+++ 05.Week/02.óra.php
@@ -2,5 +2,19 @@
 
 echo "<h1> 5.Hét";
 echo "<h2> 2.Óra";
+/*
+
+01.Feladat:
+    Készíts egy 2D-s tömböt. Helyezz el benne 20 tömböt a következő indexekkel:
+        - Keresztnév
+        - Vezetéknév
+        - Év
+        - Hónap 
+        - Nap
+
+A nevekhez hanszáld az előző órai listákat. Az év egy random szám legyen 2000 és 2005 között. 
+A hónap egy random szám legyen 1 és 12 között. A nap legyen egy random szám 1 és 28 között.
+
+*/
 echo "<h3> 1.Feladat";
 
