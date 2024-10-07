88ea9e6c05927d4f59b5b7778b6e0ca4c860af5e
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-10-07 13:50:28 +0200

    Órai Munka
    MIAFASZ, teháthogy Mi a fasz ._.

--- 5. het/index.php
+++ 5. het/index.php
@@ -1,127 +1,64 @@
-<?php 
+<?php
 
-	// $lista = array();
-	
-	// array_push($lista, array());
-	// array_push($lista, array());
-	// array_push($lista, array());
-	// array_push($lista, array());
-	// array_push($lista, array());
-	
-	// print_r($lista);
-	
-	// 2D tömb
-	// $tomb = array();
-	
-	// for($i=0;$i<5;$i++){
-		
-		// $uj_tomb = array();
-		
-		// for($j=0;$j<5;$j++){
-			
-			// array_push($uj_tomb, $j);
-			
-		// }
-		
-		// array_push($tomb, $uj_tomb);
-		
-	// }
-	
-	// print_r($tomb);
-	
-	// print_r($tomb[0])
-	
-	// echo $tomb[1][3];
-	
-	// Feladat:
-	
-	// Készíts egy tömböt 10 tömbbel. Minden belső tömböt tölts fel 10 darab random számmal 0 és 10 között
-	
-	// $tomb = array();
-	
-	// for($i=0;$i<10;$i++){
-		
-		// $uj_tomb = array();
-		
-		// for($j=0;$j<10;$j++){
-			
-			// $randomszam = rand(0, 10);
-			
-			// array_push($uj_tomb, $randomszam);
-			
-		// }
-		
-		// array_push($tomb, $uj_tomb);
-		
-	// }
-	
-	// print_r($tomb);
+echo "<h1> 5.Hét </h1>";
+echo "<h2> 1.feladat </h2>";
 
+$kulso_tomb = array();
 
+for($i = 0; $i < 10; $i++) {
+    $belso_tomb = array();
+    for($j = 0; $j < 10; $j++) {
+        $randomszam = rand(0, 50);
 
-	// $tomb{
-			
-			// [0] => tomb{
-				
-				// [0] => nev[$i]
-				// [1] => tomb{
-					
-					// [0] => 1
-					// [1] => 3
-					// [2] => 5
-					// [3] => 4
-					// [4] => 2
-					
-				// }
-				
-			// }
-			
-		// }
+        array_push($belso_tomb, $randomszam);
+        
+    }
 
-	$nevek = array("András", "Balázs", "Cecília", "Dénes");
-	
-	$tomb = array();
-	
-	for($i=0;$i<count($nevek);$i++){
-		
-		$uj_tomb = array();
-		
-		array_push($uj_tomb, $nevek[$i]);
-		
-		$szamok = array();
-		
-		for($j=0;$j<5;$j++){
-			
-			$randomszam = rand(1, 5);
-			
-			array_push($szamok, $randomszam);
-			
-		}
-		
-		array_push($uj_tomb, $szamok);
-		
-		array_push($tomb, $uj_tomb);
-		
-	}
-	
-	print_r($tomb);
-	
-	// print_r($tomb);
-	
-	// $nevek = array("nev" => "András", "szul_datum" => "2001.07.28");
-	
-	// print_r($nevek);
-	
-	// echo $nevek['szul_datum'];
-	
-	// $nevek = array();
-	
-	// $nevek['nev'] = "András"; // array_push()
-	
-	// print_r($nevek);
-	
-	
-	
-	
-	
-?>
+    array_push($kulso_tomb, $belso_tomb);
+
+}
+
+print_r($kulso_tomb);
+
+echo "<h2> 2.feladat </h2>";
+
+$vezeteknevek = array("Nagy", "Kovács", "Tóth", "Szabó", "Horváth", "Varga", "Kiss")
+$keresztnevek = array("Dénes", "Géza", "Andárs", "Péter", "Zsolt", "János")
+
+$kulso_tomb = array();
+
+for($i = 0; $i < 10; $i++) {
+
+    $belso_tomb = array();
+
+    $randindex = rand(0, count($vezeteknevek) = $vezeteknevek[$randindex])
+
+    $belso_tomb['keresztnev'] = $keresztnevek[$randindex];
+
+    $belso_tomb['eletkor'] = rand(1, 50);
+
+    array_push($kulso_tomb, $belso_tomb);
+}
+
+print_r($kulso_tombs);
+echo "<br>";
+
+echo "<h2> 2.feladat </h2>";
+
+$huszonotalatt = 0;
+
+for($i = 0; $i < 10; $i++) {
+    if($kulso_tomb[$i]['eletkor'] < 25) {
+        $huszonotalatt++
+    }
+}
+
+echo "$huszonotalatt személy fiatalabb 25-nél";
+
+echo "<h2> 2.feladat </h2>";
+
+for($i = 0; $i < 10; $i++) {
+    $kulso_tomb[$i]['eletkor'] += rand(1, 10)
+}
+
+print_r($kulso_tomb);
