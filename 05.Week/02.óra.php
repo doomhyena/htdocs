a684a21623f36ffd6290fc9012d03f4bddb320f1
Csontos Kincső <91425554+doomhyena@users.noreply.github.com>
2024-10-07 15:34:39 +0200

    Pontosvessző

--- 05.Week/02.óra.php
+++ 05.Week/02.óra.php
@@ -1,6 +1,5 @@
 <?php // 2024/10/07
 
-echo "<h1> 5.Hét <\h1>"
-echo "<h2> 2.Óra <\h>"
-echo "<h3> 1.Feladat <\h3>"
-
+echo "<h1> 5.Hét <\h1>";
+echo "<h2> 2.Óra <\h>";
+echo "<h3> 1.Feladat <\h3>";
